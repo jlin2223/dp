117d1b5e973d31ae2d27267b07087969b1430618
jlin <coolljp15@hotmail.com>
2018-05-07 17:24:29 +1000

    mini3 updated

--- mlp_o_m_d_s_e_mini3.py
+++ mlp_o_m_d_s_e_mini3.py
@@ -1,19 +1,21 @@
 import numpy as np
 import h5py
 import math
+import time
+import sys
 
-with h5py.File('/Users/david/PycharmProjects/Assignment1/data/train_128.h5','r') as H:
+with h5py.File('E://David//学习//Semester3//COMP5329//ass1//Assignment-1-Dataset//Assignment-1-Dataset//train_128.h5','r') as H:
     data = np.copy(H['data'])
     #print(data.shape[0])
     #np.savetxt('/Users/david/PycharmProjects/Assignment1/data/train_128.csv', data, delimiter=',')
-with h5py.File('/Users/david/PycharmProjects/Assignment1/data/train_label.h5','r') as H_l:
+with h5py.File('E://David//学习//Semester3//COMP5329//ass1//Assignment-1-Dataset//Assignment-1-Dataset//train_label.h5','r') as H_l:
     data_label = np.copy(H_l['label'])
     #print(data_label.shape)
 
-data_sample = np.zeros([1000, data.shape[1]])
-data_sample_label = np.zeros(1000)
+data_sample = np.zeros([10000, data.shape[1]])
+data_sample_label = np.zeros(10000)
 i = 0
-while i < 1000:
+while i < 10000:
 
     it = np.random.randint(data.shape[0])
     data_sample[i] = data[it]
@@ -33,7 +35,7 @@ class Activation(object):
         # a = np.tanh(x)
         return 1.0 - a**2
     def __relu(self, x):
-        return np.maximum(0.001 * x, 0.5 * x)
+        return np.maximum(0 , 0.05 * x)
 
     def __relu_deriv(self, a):
         dx = np.ones_like(a)
@@ -166,7 +168,8 @@ class HiddenLayer(object):
               # print('self.W', self.W.shape)
               mode = dropout_param['mode']
               if mode == 'train':
-                    self.grad_W = np.atleast_2d(mask * self.input).T.dot(np.atleast_2d(delta)) + lamdb * (mask * self.W.T).T / len(self.input) #atleast_2d: transfer data into 2 dimension
+                    #self.grad_W = np.atleast_2d(mask * self.input).T.dot(np.atleast_2d(delta)) + lamdb * (mask * self.W.T).T / len(self.input) #atleast_2d: transfer data into 2 dimension
+                    self.grad_W = np.atleast_2d(mask * self.input).T.dot(np.atleast_2d(delta))
               elif mode == 'test':
                   self.grad_W = np.atleast_2d(self.input).T.dot(np.atleast_2d(delta)) + lamdb * self.W / len(
                       self.input)  # atleast_2d: transfer data into 2 dimension
@@ -175,10 +178,14 @@ class HiddenLayer(object):
               #self.grad_W = self.dropout_backward(self.grad_W)
               #self.grad_W = np.atleast_2d(self.input).T.dot(np.atleast_2d(delta))
               self.grad_b = delta
-
+              #print('grad_W =', self.grad_W)
+              #print('grad_b =', self.grad_b)
               d = delta.dot(self.W.T)
-              delta_ = d * self.activation_deriv(self.input)
 
+              delta_ = d * self.activation_deriv(self.input)
+              # print('grad_W size', sys.getsizeof(self.grad_W))
+              # print('grad_b size=', sys.getsizeof(self.grad_b))
+              # print('delta_ size', sys.getsizeof(delta_))
               return delta_
 
 
@@ -193,7 +200,7 @@ class MLP:
               self.params = []
               self.W = []
               self.activation = activation
-
+              self.mini_size = 0
 
 
               print('number of layer:', len(layers))
@@ -216,16 +223,21 @@ class MLP:
               return output
 
        def softmax_predict(self, X):
-           exp_scores = np.exp(X-np.max(X))
+           #print('X - np.max(X) = ', X-np.max(X))
+           exp_scores = np.exp(X-np.max(X)) # np.max(X) 是 inf
+
            #print('exp_scores.shape = ', exp_scores.shape)
            return exp_scores / np.sum(exp_scores)
 
        def loss(self, X, y):
            num_examples = X.shape[0]
+           #print('X size = ', X.shape)
+           #print('X = ', X)
            #print('y_hat.shape = ', X.shape)
            probs = self.softmax_predict(X)
-
+           #print('probs = ',probs)
            correct_logprobs = -np.log(probs[y])
+           #print('correct_logprobs = ',correct_logprobs)
            #data_loss = np.sum(corect_logprobs)
            return correct_logprobs
 
@@ -239,17 +251,41 @@ class MLP:
               activation_deriv = Activation(self.activation).f_deriv
               y = np.int(y)
 
+              regu_start_time = None
+              regu_end_time = None
+              loss_start_time = None
+              loss_end_time = None
+              delta_start_time = None
+              delta_end_time = None
+
               # L2 regularization cost
-              l_W = []
-              for w in self.W:
-                  l_w = np.sum(np.dot(w, w.T))
-                  l_W.append(l_w)
-              regularization_cost = lambd * np.sum(l_W) /len(y_hat)/2
+              # regu_start_time = time.time()
+              # l_W = []
+              # for w in self.W:
+              #     l_w = np.sum(np.dot(w,w.T))
+              #     print('l_w size = ', l_w)
+              #     l_W.append(l_w)
+              # regularization_cost = lambd * np.sum(l_W) /len(y_hat)/2
+              # regu_end_time = time.time()
+              # print('regu duration = ', regu_end_time - regu_start_time)
 
               # MSE
-              loss = self.loss(y_hat, y) + regularization_cost
+              loss_start_time = time.time()
+              #loss = self.loss(y_hat, y) + regularization_cost
+              loss = self.loss(y_hat, y)
+              loss_end_time = time.time()
+              #print('loss duration = ', loss_end_time - loss_start_time)
 
+              #loss = self.loss(y_hat, y)
+              delta_start_time = time.time()
               delta = self.diff(y_hat, y)
+              delta_end_time = time.time()
+              #print('delta duration = ', delta_end_time - delta_start_time)
+              #print('delta', delta)
+
+              #print('loss', sys.getsizeof(loss))
+              #print('delta', sys.getsizeof(delta))
+
               #error = y_hat[y] - np.max(y_hat)
               #loss = np.sum(error ** 2) /len(y_hat) + regularization_cost
               #loss = np.sum(error ** 2) / len(y_hat)
@@ -267,10 +303,14 @@ class MLP:
        def update_mini(self,learning_rate, momemtum_rate):
               for layer in self.layers:
                      #print('grad_W', layer.grad_W)
-                     layer.v_w = momemtum_rate * layer.v_w + (1-momemtum_rate) * layer.grad_W
-                     layer.v_b = momemtum_rate * layer.v_b + (1-momemtum_rate) * layer.grad_b
+                     layer.v_w = momemtum_rate * layer.v_w + (1-momemtum_rate) * layer.grad_W / self.mini_size
+                     layer.v_b = momemtum_rate * layer.v_b + (1-momemtum_rate) * layer.grad_b /self.mini_size
                      layer.W -= learning_rate * layer.v_w
                      layer.b -= learning_rate * layer.v_b
+                     # print('layer.v_w size', sys.getsizeof(layer.v_w))
+                     # print('layer.v_b size', sys.getsizeof(layer.v_b))
+                     # print('layer.W size', sys.getsizeof(layer.W))
+                     # print('layer.b size', sys.getsizeof(layer.b))
                      #print('grad_b', layer.grad_b)
 
        def random_mini_batches(self, X, y, mini_batch_size=16, seed_m=0):
@@ -309,17 +349,20 @@ class MLP:
 
 
        def fit(self, X, y, learning_rate=0.1, epochs=100, lamdb=0.01, momemtum_rate=0.5, dropout_param={'p':0.5 ,'mode':'train','seed':6},mini_batch_size=64):
-
+            self.mini_size = mini_batch_size
             X = np.array(X)
             y = np.array(y)
             to_return = 0
             seed_m=10
-            average_loss = 0
+
             for k in range(epochs):
+                    average_loss = 0
                     seed_m = seed_m + 1
                     minibatches = self.random_mini_batches(X, y, mini_batch_size, seed_m)
 
-
+                    for l in self.layers:
+                        l.grad_W = 0
+                        l.grad_b = 0
                     mini_count = 1
                     for minibatch in minibatches:
 
@@ -327,23 +370,39 @@ class MLP:
                         # Select a minibatch
                         (minibatch_X, minibatch_y) = minibatch
 
-                        loss = np.zeros(minibatch_X.shape[
-                                            0])  # X.shape[0] denotes the number of data and x.shape[1] denotes the dimension of data
+                        #loss = np.zeros(minibatch_X.shape[0])  # X.shape[0] denotes the number of data and x.shape[1] denotes the dimension of data
+                        loss = 0.0
                         #print('minibatch_x', minibatch_X)
                         #print('minibatch_y', minibatch_y)
                         for it in range(minibatch_X.shape[0]):
+                                forward_start_time = None
+                                forward_end_time = None
+                                backward_start_time = None
+                                backward_end_time = None
+                                mse_start_time = None
+                                mse_end_time = None
+
                                 i = np.random.randint(minibatch_X.shape[0]) # it will iterate all the data in an array
 
                             # forward pass
+                                forward_start_time = time.time()
                                 y_hat = self.forward(minibatch_X[i], dropout_param)
+                                forward_end_time = time.time()
+                                #print('forward duration = ', forward_end_time - forward_start_time)
 
+                                mse_start_time = time.time()
+                                loss, delta = self.criterion_MSE(minibatch_y[:,i], y_hat, lamdb)
+                                mse_end_time = time.time()
+                                #print('mse duration = ', mse_end_time - mse_start_time)
                             # backward pass
-                                loss[it], delta = self.criterion_MSE(minibatch_y[:,i], y_hat, lamdb)
+                                backward_start_time = time.time()
                                 self.backward(delta, lamdb)
+                                backward_end_time = time.time()
+                                #print('backward duration = ', backward_end_time - backward_start_time)
                                 self.update(learning_rate)
-                                print('finish epoch:',k,'batch:', mini_count,'data:',it)
+                                #print('finish epoch:',k,'batch:', mini_count,'data:',it)
 
-                        average_loss += loss
+                                average_loss += loss
                             #print('loss[',it,']=',loss[it])
                         mini_count += 1
                     # update
@@ -351,6 +410,8 @@ class MLP:
 
                         self.update_mini(learning_rate, momemtum_rate)
 
+                    print('epochs = ', k, 'loss = ', average_loss/X.shape[0])
+                            #print("layer ",i, "size = ", self.layers[i].W.shape)
             to_return = (average_loss) / X.shape[0]
 
             return to_return
@@ -370,12 +431,12 @@ class MLP:
 ### Try different MLP models
 
 
-nn = MLP([128,20,10], 'relu')     # auto initialize three class: MLP, HiddenLayer, Activation
+nn = MLP([128,90,50,10], 'relu')     # auto initialize three class: MLP, HiddenLayer, Activation
 input_data = data_sample
 output_data = data_sample_label
 ### Try different learning rate and epochs
 
-MSE = nn.fit(input_data, output_data, learning_rate=0.001, epochs=1, lamdb=0.5, momemtum_rate=0.9,dropout_param={'p':0.3 ,'mode':'train','seed':5},mini_batch_size=100)
+MSE = nn.fit(input_data, output_data, learning_rate=0.001, epochs=1, lamdb=10, momemtum_rate=0.99,dropout_param={'p':0.5 ,'mode':'train','seed':5},mini_batch_size=128)
 
 
 
@@ -384,7 +445,8 @@ output = nn.predict(input_data)
 
 print('predict:',output)
 print('answer:',output_data)
-print('loss:%f'%MSE[-1])
+print('loss:%f'%MSE)
+print('loss standard:%f'%np.log(10))
 m = output == output_data
 correct = np.sum(m==True)
 print('accuracy: %.2f%%'%((correct/output_data.shape)*100))
